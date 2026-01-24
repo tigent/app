7c4f62e9bd76ef9e808617b583091f2b2d074871
josh <josh@afterima.ge>
2026-01-24 23:10:31 +0000

    feat: remove redis and workflow dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from "next";
-import { withWorkflow } from "workflow/next";
 
 const config: NextConfig = {};
 
-export default withWorkflow(config);
+export default config;
