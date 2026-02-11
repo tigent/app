c56cd05c618fe8f2eb5c802c3d3ecb67961e79b7
josh <josh@afterima.ge>
2026-02-11 02:14:07 +0000

    feat: dynamic tab titles for dashboard pages

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,9 @@
+import type { Metadata } from 'next';
 import { Empty } from './components/empty';
+
+export const metadata: Metadata = {
+  title: 'Tigent - Dashboard',
+};
 
 export default function Page() {
   return <Empty hasrepos />;
