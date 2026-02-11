bdbfdfb9977075c92654ec1079301d115fc0282b
josh <josh@afterima.ge>
2026-02-11 08:43:09 +0000

    fix: add repo scope to oauth, revert to user token

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -6,7 +6,7 @@ export async function GET(req: NextRequest) {
   const protocol = host.includes('localhost') ? 'http' : 'https';
   const params = new URLSearchParams({
     client_id: process.env.GITHUB_CLIENT_ID!,
-    scope: 'read:org',
+    scope: 'read:org repo',
     redirect_uri: `${protocol}://${host}/api/auth/callback`,
   });
   redirect(`https://github.com/login/oauth/authorize?${params}`);
